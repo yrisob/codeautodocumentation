c34cf0cd59f8eb39fc72124ec7a0b813b5924d0f
Yura Sobolev <yrisob@gmail.com>
2017-09-15 11:41:03 +0300

    docreader

--- main.go
+++ main.go
@@ -1,38 +1,10 @@
 package main
 
 import (
-	"regexp"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"strings"
 	"./topicBlock"
 )
 
-type attribute string
-
-// ReadAllFilesFromDir получение всех вложенных файлов директории
-func ReadAllFilesFromDir(dirName string) []string {
-	fmt.Println("Read dir:", dirName)
-	var fileList []string
-	files, error := ioutil.ReadDir(dirName)
-
-	if error != nil {
-		log.Fatal(error)
-	}
-
-	for _, file := range files {
-		if file.IsDir() {
-			if strings.Index(file.Name(), ".") != 0 {
-				fileList = append(fileList, ReadAllFilesFromDir(fmt.Sprintf("%s/%s", dirName, file.Name()))...)
-			}
-		} else {
-			fileList = append(fileList, file.Name())
-		}
-	}
-
-	return fileList
-}
 
 var someText=`some text
 //@router[path="/people/{id}"]
@@ -47,19 +19,9 @@ some footertext`
 //@response[answer={id:1, name: "someName", age:12, gender: "M"}]
 //@description[где id- идентификатор персоны, name - имя персоны, age- возраст персоны, gende- гендер]
 
-var regSearchAttr = regexp.MustCompile(`(//@\b(router)(.*)(\n)*)?(//@\b(method)(.*)(\n)*)(//@\b(router)(.*)(\n)*)?(//@\b(response)(.*)(\n)*)?(//@\b(request)(.*)(\n)*)?(//@\b(response)(.*)(\n)*)?(//@\b(description)(.*)(\n)*)?`)
 
 func main() {
-	var stringSlices = regSearchAttr.FindAllStringIndex(someText,1)
-	fmt.Println(stringSlices)
-
-	tb:= topicBlock.TopicBlock{}
-	tb.GetBlocksFromContent(someText,"test")
-
-	fmt.Println(tb)
-
-	// fmt.Println("start read directory")
-	// files := ReadAllFilesFromDir("../")
-	// fmt.Println(files)
-
+	docNavigator := topicBlock.CodeDocumentation{}
+	docNavigator.GetDocumentationFromFilesPath("../go_test")
+	fmt.Println(docNavigator)
 }
--- topicBlock/docNavigator.go
+++ topicBlock/docNavigator.go
@@ -33,28 +33,38 @@ func ReadAllFilesFromDir(dirName string) []string {
 	return fileList
 }
 
-func (cd *CodeDocumentation) GetDocumentationFromFilesPath(rootPath string){
-	cd.DateCreate = time.Now()
-	projectFiles := ReadAllFilesFromDir(rootPath)
-	cd.DocumentationBlocks = []TopicBlock{}
+func ReadDocumentationFromFile(fileName string, tb chan *TopicBlock){
+	contentbytes, error := ioutil.ReadFile(fileName)
 
-	for _, fileName := range projectFiles{
-		contentbytes, error := ioutil.ReadFile(fileName)
-
-		if error != nil{
-			log.Fatal(error)
-		}
-
-		contentString := string(contentbytes)
-		topicBlock:= TopicBlock{}
+	if error != nil{
+		log.Fatal(error)
+	}else{
+		contentString:= string(contentbytes)
 		lastIndexPoint:= strings.LastIndex(fileName, ".")
 		if lastIndexPoint <= 0{
 			lastIndexPoint = len(fileName)
 		}
+		topicBlock:= TopicBlock{}
 		topicBlock.GetBlocksFromContent(contentString, fileName[strings.LastIndex(fileName,"/")+1: lastIndexPoint])
+		tb <- &topicBlock
+	}
 
-		if topicBlock.TopicName != ""{
-			cd.DocumentationBlocks = append(cd.DocumentationBlocks, topicBlock)
+}
+
+func (cd *CodeDocumentation) GetDocumentationFromFilesPath(rootPath string){
+	cd.DateCreate = time.Now()
+	projectFiles := ReadAllFilesFromDir(rootPath)
+	cd.DocumentationBlocks = []TopicBlock{}
+	topicBlock := make(chan *TopicBlock)
+
+	for _, fileName := range projectFiles{
+		go ReadDocumentationFromFile(fileName, topicBlock)
+	}
+
+	for i:=0; i<len(projectFiles);i++{
+		tb:= <-topicBlock
+		if tb.TopicName != ""{
+			cd.DocumentationBlocks = append(cd.DocumentationBlocks, *tb)
 		}
 	}
 
